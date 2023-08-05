6712a7119ba6beef028bfc09c814e22f2f7e53bf
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-08-05 16:44:25 +0530

    GCD

---
+++ Lab2/Q3GCD.cpp
@@ -0,0 +1,6 @@
+// 2.3 Aim of the experiment: Write a function gcd(k) which returns the GCD of kth and (k+l)th
+// Fibonacci numbers for k>l. Use Euclid's algorithm to calculate the GCD. Count the number of
+// times the GCD loop executes for different values of k and record your observations in the
+// Input: Enter k : 10
+// Output: 10th and 11 th Fibonacci numbers : 34,55
+// GCD = 1
