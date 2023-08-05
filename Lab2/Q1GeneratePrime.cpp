9cb0f58fb156a8f0c1fe04a8c5f294953437dbf1
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-08-05 17:21:38 +0530

    Added the complexity of both loop

--- Lab2/Q1GeneratePrime.cpp
+++ Lab2/Q1GeneratePrime.cpp
@@ -6,12 +6,16 @@ using namespace std;
 void genratePrime(int n)
 {
     int i, j, flag;
+    int outer_loop = 0, inner_loop = 0;
+    float c1, c2;
     cout << "Prime numbers between 1 to " << n << " are: ";
     for (i = 2; i <= n; i++)
     {
+        outer_loop++;
         flag = 0;
         for (j = 2; j <= i / 2; j++)
         {
+            inner_loop++;
             if (i % j == 0)
             {
                 flag = 1;
@@ -21,6 +25,12 @@ void genratePrime(int n)
         if (!flag)
             cout << i << " ";
     }
+    c1 = (float)outer_loop / (float)n;
+    c2 = (float)inner_loop / (float)n;
+    cout << endl;
+    cout << "Outer loop executes " << outer_loop << " times and inner loop executes " << inner_loop << " times.";
+    cout << "\nComplexity of outer loop: " << c1;
+    cout << "\nComplexity of inner loop: " << c2;
 }
 
 int main(int argc, char const *argv[])
