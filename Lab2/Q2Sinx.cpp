3b66e308cfc65580f2b8e6f922effec677e8bfcf
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-08-05 16:31:42 +0530

    Sinx of a value upto d decimal spaces

---
+++ Lab2/Q2Sinx.cpp
@@ -0,0 +1,30 @@
+// 2.2 Aim of the experiment: Write a function sine(x, d) that returns the sine of input x accurately
+// upto d decimal places computed using Taylor's series approximation of sine.
+// Input: Enter x - 25.56
+// Enter decimal places - 3
+// Output: 0.431
+#include <bits/stdc++.h>
+using namespace std;
+double sine(double x, int d)
+{
+    double sum = 0, term = x, eps = pow(10, -d - 1);
+    int i = 1;
+    while (abs(term) > eps)
+    {
+        sum += term;
+        term = -term * x * x / (2 * i * (2 * i + 1));
+        i++;
+    }
+    return sum;
+}
+int main()
+{
+    double x;
+    int d;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter decimal places: ";
+    cin >> d;
+    cout << "Sine of " << x << " upto " << d << " decimal places is: " << sine(x, d) << endl;
+    return 0;
+}
